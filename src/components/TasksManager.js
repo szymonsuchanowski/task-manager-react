8d8d9ceea8313299ff1e8199588608493c2bb477
Szymon Suchanowski <szymonsuchanowski@gmail.com>
2021-11-22 20:00:27 +0100

    feat: add method rendering form structure

--- src/components/TasksManager.js
+++ src/components/TasksManager.js
@@ -3,16 +3,43 @@ import React from 'react';
 class TasksManager extends React.Component {
     state = {
         tasks: [],
+        newTaskTitle: '',
     }
 
     onClick = () => {
         const { tasks } = this.state;
-        console.log( tasks)
+        console.log(tasks)
     }
 
     render() {
-        return <h1 onClick={ this.onClick }>TasksManager</h1>
-    }
-}
+        const { newTaskTitle } = this.state;
+        return (
+            <div className='tasks'>
+                <h1 className='tasks__header'>Manage your tasks!</h1>
+                <article className='tasks__form'>
+                    <form className='form' onSubmit={this.submitHandler}>
+                        <div className='form__row'>
+                            <label>
+                                Add new task
+                                <input className='form__field'
+                                    name='task' value={newTaskTitle}
+                                    onChange={this.inputChange}
+                                    placeholder='type task here' />
+                                <span className='form__field-border'></span>
+                            </label>
+                        </div>
+                        <div className='form__row'>
+                            <input className='form__submit btn'
+                                value='Add'
+                                type='submit' />
+                        </div>
+                    </form>
+                </article>
+            </div>
+        );
+    };
+
+    
+};
 
 export default TasksManager;
